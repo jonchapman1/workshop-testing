67d08bec42de0e3e08b4b526349099a33960c82a
Jon Chapman <chapman@maths.ox.ac.uk>
2018-10-30 14:28:34 +0000

    removed coulomb_failed

--- test_mysoftware.py
+++ test_mysoftware.py
@@ -15,10 +15,3 @@ def test_coulomb():
     assert coulomb(10) == 0.1
 
 
-
-def test_coulomb_failed():
-    try:
-        coulomb(0.0)
-        raise Exception("Test failed")
-    except:
-        pass
