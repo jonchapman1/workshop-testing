7fea9887f3f7a243403954fc9ede8a09ebd38a81
Jon Chapman <chapman@maths.ox.ac.uk>
2018-10-30 16:31:11 +0000

    updated functions with documentation

--- mysoftware/functions.py
+++ mysoftware/functions.py
@@ -1,5 +1,27 @@
 
 def square(x):
+    """
+    Takes a number x and squares it.
+
+    Parameters:
+    -----------
+    x, float or int:
+        Number which is to be squared.
+
+    Returns:
+    --------
+    float:
+        Square of x.
+
+    Examples:
+    ---------
+    >>> square(2)
+    4
+
+    >>> square(-1)
+    1
+
+    """
     return x*x
 
 def coulomb(x):
